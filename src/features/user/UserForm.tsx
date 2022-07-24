bd829a9668781608c2f9789f9752c5ead69610da
Jonathan <weissjon@gmail.com>
2022-07-24 10:28:33 +0300

    style the page

--- src/features/user/UserForm.tsx
+++ src/features/user/UserForm.tsx
@@ -1,18 +1,9 @@
 import React, { ChangeEvent, useState, useEffect } from "react";
 
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import {
-  decrement,
-  // increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectUsername,
-  selectPhoneNumber,
-  setUser,
-} from "./userSlice";
+import { selectUsername, selectPhoneNumber, setUser } from "./userSlice";
 import styles from "./UserForm.module.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, ErrorMessage } from "formik";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
@@ -43,10 +34,9 @@ export function UserForm() {
   const username = useAppSelector(selectUsername);
   const phoneNumber = useAppSelector(selectPhoneNumber);
   const dispatch = useAppDispatch();
-  const [incrementAmount, setIncrementAmount] = useState("2");
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const MAX_PASSWORD_LENGTH = 12;
 
   const validateForm = (formData: UserFormData) => {
     const errors: UserFormErrors = {};
@@ -54,7 +44,8 @@ export function UserForm() {
     if (!formData.username) {
       errors.username = "Required";
     } else if (
-      // double check, for staying bullet proof when we change the input element's maxLength attribute value
+      // in addition to the maxLength restriction applied to the input DOM element),
+      // double check the length for staying bullet proof in case we change the input element's maxLength attribute value
       formData.username.length > 24
     ) {
       errors.username = "Up to 24 character allowed";
@@ -96,9 +87,7 @@ export function UserForm() {
     return errors;
   };
 
-  const handleTabChange = (ev: ChangeEvent<{}>, tabIndex: any): void => {
-    setSelectedTabIndex(tabIndex);
-  };
+  const handleTabChange = (ev: ChangeEvent<{}>, tabIndex: any): void => {};
 
   function TabPanel(props: TabPanelProps) {
     const { children, value, index, ...other } = props;
@@ -107,168 +96,170 @@ export function UserForm() {
       <div
         role="tabpanel"
         hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
+        id={`user-tabpanel-${index}`}
+        className={styles.userTabPanel}
+        aria-labelledby={`user-tab-${index}`}
         {...other}
       >
-        {value === index && <Box p={3}>{children}</Box>}
+        {value === index && (
+          <Box className={styles.formContainer} p={3}>
+            {children}
+          </Box>
+        )}
       </div>
     );
   }
 
-  useEffect(() => {}, []);
-
   return (
-    <div>
-      <div className={styles.row}>
-        <div>
-          <h1>User Details</h1>
+    <div className={styles.row}>
+      <div className={styles.tabsContainer}>
+        <h1>User Details</h1>
 
-          <Tabs
-            value={selectedTabIndex}
-            onChange={handleTabChange}
-            indicatorColor="primary"
-            textColor="primary"
-            variant="fullWidth"
-            aria-label="user form and details tabs"
+        <Tabs
+          className={styles.tabs}
+          value={selectedTabIndex}
+          onChange={(ev, tabIndex) => setSelectedTabIndex(tabIndex)}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="user form and details tabs"
+        >
+          <Tab
+            id="userFormTab"
+            label="Form"
+            aria-controls="tab-panel--user-form"
+          />
+          <Tab
+            id="userDetailsTab"
+            label="User"
+            aria-controls="tab-panel--user-details"
+          />
+        </Tabs>
+
+        <TabPanel value={selectedTabIndex} index={0}>
+          <Formik
+            initialValues={{
+              username,
+              phoneNumber,
+              password: "qweQWE!",
+              passwordConfirmation: "qweQWE!",
+            }}
+            validate={validateForm}
+            onSubmit={async (formData, { setSubmitting }) => {
+              try {
+                const { username, password, phoneNumber } = formData;
+                setSubmitting(true);
+                dispatch(setUser({ username, password, phoneNumber }));
+              } catch (err) {
+                console.error(err);
+              } finally {
+                setSubmitting(false);
+              }
+            }}
           >
-            <Tab
-              id="userFormTab"
-              label="Form"
-              aria-controls="tab-panel--user-form"
-            />
-            <Tab
-              id="userDetailsTab"
-              label="User"
-              aria-controls="tab-panel--user-details"
-            />
-          </Tabs>
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <form onSubmit={handleSubmit} className={styles.form}>
+                <ul>
+                  <li className={styles.row}>
+                    <TextField
+                      id="username"
+                      value={values.username}
+                      name="username"
+                      label="Username"
+                      variant="outlined"
+                      inputProps={{ maxLength: 24 }}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
 
-          <TabPanel value={selectedTabIndex} index={0}>
-            <Formik
-              initialValues={{
-                username,
-                phoneNumber,
-                password: "qweQWE!",
-                passwordConfirmation: "qweQWE!",
-              }}
-              validate={validateForm}
-              onSubmit={async (formData, { setSubmitting }) => {
-                try {
-                  const { username, password, phoneNumber } = formData;
-                  setSubmitting(true);
-                  dispatch(setUser({ username, password, phoneNumber }));
-                } catch (err) {
-                  console.error(err);
-                } finally {
-                  setSubmitting(false);
-                }
-              }}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <ul>
-                    <li className={styles.row}>
-                      <TextField
-                        id="username"
-                        value={values.username}
-                        name="username"
-                        label="Username"
-                        variant="outlined"
-                        inputProps={{ maxLength: 24 }}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
+                    {errors.username && touched.username && errors.username}
+                  </li>
 
-                      {errors.username && touched.username && errors.username}
-                    </li>
+                  <li className={styles.row}>
+                    <TextField
+                      id="phoneNumber"
+                      name="phoneNumber"
+                      value={values.phoneNumber}
+                      type="number"
+                      label="Phone Number"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{ maxLength: 10 }}
+                      variant="outlined"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
 
-                    <li className={styles.row}>
-                      <TextField
-                        id="phoneNumber"
-                        name="phoneNumber"
-                        type="number"
-                        label="Phone Number"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        inputProps={{ maxLength: 10 }}
-                        variant="outlined"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
+                    <ErrorMessage name="phoneNumber" component="div" />
+                  </li>
 
-                      <ErrorMessage name="phoneNumber" component="div" />
-                    </li>
+                  <li className={styles.row}>
+                    <TextField
+                      id="password"
+                      name="password"
+                      type="password"
+                      label="Password"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{ maxLength: MAX_PASSWORD_LENGTH }}
+                      variant="outlined"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <ErrorMessage name="password" component="div" />
+                  </li>
 
-                    <li className={styles.row}>
-                      <TextField
-                        id="password"
-                        name="password"
-                        type="password"
-                        label="Password"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        inputProps={{ maxLength: 12 }}
-                        variant="outlined"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                      <ErrorMessage name="password" component="div" />
-                    </li>
+                  <li className={styles.row}>
+                    <TextField
+                      id="passwordConfirmation"
+                      name="passwordConfirmation"
+                      type="password"
+                      label="Repeat Password"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{ maxLength: 12 }}
+                      variant="outlined"
+                    />
 
-                    <li className={styles.row}>
-                      <TextField
-                        id="passwordConfirmation"
-                        name="passwordConfirmation"
-                        type="password"
-                        label="Repeat Password"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        inputProps={{ maxLength: 12 }}
-                        variant="outlined"
-                      />
+                    <ErrorMessage name="passwordConfirmation" component="div" />
+                  </li>
+                </ul>
 
-                      <ErrorMessage
-                        name="passwordConfirmation"
-                        component="div"
-                      />
-                    </li>
-                  </ul>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={isSubmitting}
+                  className={styles.submitBtn}
+                >
+                  Submit
+                </Button>
+              </form>
+            )}
+          </Formik>
+        </TabPanel>
 
-                  <Button
-                    variant="contained"
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    Submit
-                  </Button>
-                </form>
-              )}
-            </Formik>
-          </TabPanel>
-
-          <TabPanel value={selectedTabIndex} index={1}>
-            <div>
-              <h3>
-                User Name: <span className="value-col">{username}</span>
-              </h3>
-              <h4>
-                Phone number: <span className="value-col">{phoneNumber}</span>
-              </h4>
-            </div>
-          </TabPanel>
-        </div>
+        <TabPanel value={selectedTabIndex} index={1}>
+          <div>
+            <h3>
+              User Name: <span className="value-col">{username}</span>
+            </h3>
+            <h4>
+              Phone number: <span className="value-col">{phoneNumber}</span>
+            </h4>
+          </div>
+        </TabPanel>
       </div>
     </div>
   );
